0ff5e84de3b0fe7a7924e21feeafc3a0536a86c7
jeliana <jelianaparramatias.2014@gmail.com>
2021-08-20 17:48:18 -0400

    morgan

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from "express"
-import morgan from "morgan";
 import userRoutes from "./routes/user.js";
 import docRoutes from "./routes/doc.js";
 import solRoutes from "./routes/solicitud.js";
